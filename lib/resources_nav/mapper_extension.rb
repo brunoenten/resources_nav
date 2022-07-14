9c3161de5e7b61a8e579e28cecd90a52bb1f8d0c
Bruno Enten <bruno@enten.me>
2022-07-14 17:57:01 +0300

    fixed options not being forwarded upstream

--- lib/resources_nav/mapper_extension.rb
+++ lib/resources_nav/mapper_extension.rb
@@ -5,12 +5,14 @@ module ResourcesNav
     included do
       alias_method :resources_orig, :resources
       def resources(*resources, &block)
-        options = resources.extract_options!.dup
-        if options[:nav]
+        options = resources.last
+        # From activesupport/lib/active_support/core_ext/array/extract_options.rb
+        if options.is_a?(Hash) && options.extractable_options? && options[:nav]
           @set.resources_nav ||= []
-          resources.each do |resource|
+          resources.to(-2).each do |resource|
             @set.resources_nav << ResourcesNav::Resource.new(resource, options[:nav]) unless @set.resources_nav.include?(resource)
           end
+          options.delete(:nav)
         end
         resources_orig(*resources, &block)
       end
